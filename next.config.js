a57e5a7ceb60811b1c99711af2c59fd1c17a3ffe
Bishal Sarker <mr.bishalsarker@gmail.com>
2024-03-19 01:48:10 +0600

    image loading issue fixed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {images: {
+    loader: "custom",
+    loaderFile: './ImageLoader.js'
+  }};
 
 module.exports = nextConfig
